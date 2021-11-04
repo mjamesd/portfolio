6a12e5bd98fecefa22492decc61d1e6f2a569e18
Mark Drummond <mjamesd@gmail.com>
2021-11-04 02:05:23 -0700

    Updated portfolio design

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Author: Mark Drummond
+// Date: 06-Nov-2021
+// My Portfolio
+// See README.md for more information
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+ $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
